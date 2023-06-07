82eccba3fca77ffa645b970aad67bf5cc6780cdf
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-07 16:53:26 +0530

    action type done

---
+++ src/store/actionTypes.js
@@ -0,0 +1,8 @@
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const RESET = "RESET";
+export const ADD_TODO = "ADD_TODO";
+export const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+export const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+export const GET_TODOS_ERROR = "GET_TODOS_ERROR";
+export const DELETE_TODO = "DELETE_TODO";
