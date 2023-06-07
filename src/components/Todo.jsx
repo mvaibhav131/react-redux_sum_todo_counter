e248ca5cb629fa56f3d638f7a64b984fe2502406
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-07 16:52:00 +0530

    todo done

---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteTodo } from '../store/action';
+
+const Todo = ({ id, value }) => {
+    const dispatch = useDispatch();
+    const handleDelete = () => {
+        deleteTodo(dispatch,id)
+    }
+  return (
+    <div style={{margin:"13px", display:"flex",justifyContent:"space-around"}}>
+          <h4>{value}</h4>
+          <button style={{width:"100px",height:"33px"}} onClick={handleDelete}>Delete TODO</button>
+    </div>
+  )
+}
+
+export default Todo
