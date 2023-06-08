a30d9cd2578773874ede9d8c0c22813126118dc8
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-08 12:45:25 +0530

    :+1: routes added to app file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Counter from './components/Counter';
 import Todos from './components/Todos';
+import Home from './components/Home';
+import Sum from './components/Sum';
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-      <Todos/>
+    <div className="App" >
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/counter' element={<Counter/>} />
+          <Route path='/todo' element={<Todos/>} />
+          <Route path='/sum' element={<Sum/>} />        
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
