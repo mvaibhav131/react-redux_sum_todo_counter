86d18542a89ecda6fbd048fdc61ac4217f8ce1bd
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-08 16:49:52 +0530

    :sparkles: style changes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,21 +2,23 @@
  import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decrementCounter, incrementCounter, resetCounter } from '../store/action';
+import {Button } from '@chakra-ui/react';
  
+
 const Counter = () => {
     const dispatch = useDispatch();
     const counter = useSelector(state => state.count); 
    return (
        <>
-           <div>
+           <div style={{color:"teal", fontSize:"40px"}}>
                Counter : {counter}
            </div>
            <div className="button">
-               <button onClick={()=> dispatch(incrementCounter(1))}>IncrementBy 1</button>
-               <button onClick={() => dispatch(decrementCounter(1))}>Decrement By 1</button>
-               <button onClick={() => dispatch(incrementCounter(5))}>IncrementBy 5</button>
-               <button onClick={() => dispatch(decrementCounter(3))}>Decrement By 3</button>
-               <button onClick={()=>dispatch(resetCounter(0))}>Reset</button>
+               <Button colorScheme='orange' m={3} onClick={()=> dispatch(incrementCounter(1))}>IncrementBy 1</Button>
+               <Button colorScheme='orange' m={3} onClick={() => dispatch(decrementCounter(1))}>Decrement By 1</Button>
+               <Button colorScheme='orange' m={3} onClick={() => dispatch(incrementCounter(5))}>IncrementBy 5</Button>
+               <Button colorScheme='orange' m={3} onClick={() => dispatch(decrementCounter(3))}>Decrement By 3</Button>
+               <Button colorScheme='orange' m={3} onClick={()=>dispatch(resetCounter(0))}>Reset</Button>
            </div>
 
        </>
